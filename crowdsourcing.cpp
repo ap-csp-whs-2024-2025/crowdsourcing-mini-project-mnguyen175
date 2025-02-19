cbca37a971b726297a62ebba2fe6f2903e9e9163
mnguyen175 <maryannguyen2017@gmail.com>
2025-02-19 20:58:10 +0000

    code

--- crowdsourcing.cpp
+++ crowdsourcing.cpp
@@ -13,8 +13,31 @@
 
 int main()
 {
-    vector<int> myList;
-    int userInput;
+    std::vector<std::string> song_name = {};
+    std::vector<std::string> song_artist = {};
+    std::vector<int> song_duration = {};
+
+    std::string yes_no = "y";
+    while (yes_no != "no")
+    {
+        std::cout << "Enter a song name, artist, and duration in seconds\n"
+        std::string name;
+        std::string artist;
+        int duration;
+
+        std::cin >> name >> artist >> duration;
+        append(song_name, name);
+        append(song_artist, artist);
+        append(song_duration, duration);
+
+        std::cout << "Displaying lists\n";
+        display(song_name);
+        display(song_artist);
+        display(song_duration);
+
+        std::cout << "Would you like to enter a new song (y/n?)\n";
+        std::cin >> yes_no;
+    }
 
     
     return 0;
